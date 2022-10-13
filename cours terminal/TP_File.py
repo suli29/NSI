da14462764f01e85512d7ec8be3ecd02c1b3ee9a
J3anDavid <115627033+J3anDavid@users.noreply.github.com>
2022-10-13 11:52:13 +0200

    Update TP_File.py

--- cours terminal/TP_File.py
+++ cours terminal/TP_File.py
@@ -6,24 +6,17 @@ class File:
         self.file=[nv]+self.file
     
     def defile(self):
-        """
-        Fonction qui enleve l'element du dessus de la liste
-        return : (liste) renvoie la liste avec l'element retire de la liste
-        """
         if self.est_vide():
             return None
         else:
             return self.file.pop()
         
     def est_vide(self):
-        """Methode calculant la longueur de la file"""
         return self.file == []
     
     def top(self):
-        """Methode renvoyant l'element au dessus de la file"""
         return self.file[-1]
     
     def taille (self):
-        """Methode permettant de connaitre la taille de la file"""
         return len(self.file)
     
