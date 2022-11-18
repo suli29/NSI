f7f0910654bc112dba0b6b6e60837d2ca513a7d3
J3anDavid <115627033+J3anDavid@users.noreply.github.com>
2022-11-18 09:21:46 +0100

    Update 2048 testee.py

--- projet terminal/projet 1/2048 testee.py
+++ projet terminal/projet 1/2048 testee.py
@@ -1,7 +1,7 @@
 for i in range(4):
             lignes=[]
             for j in range(4):
-                l=Label(self.zonejeu,text='',bg='azure4',
+                l=Label(self.zonejeu,text='',bg='white',
                 police=('arial',22,'bold'),width=4,height=2)
                 l.grille(ligne=i,colonne=j,padx=7,pady=7)
 
@@ -10,6 +10,11 @@ for i in range(4):
         self.zonejeu.grille()
 
     def reverse(self):
+        """
+        Fonction qui permet d'inverser les cases de ligne en ligne et de colonne en colonne
+        param i:(int) est la ligne qui est definie a 0
+        param j:(int) est la colonne qui est definie a 3
+        """
         for ind in range(4):
             i=0
             j=3
@@ -19,4 +24,7 @@ for i in range(4):
                 j-=1
 
     def transpose(self):
+        """
+        Fonction qui permet de transposer la case a une autre case
+        """
         self.grillecase=[list(t)for t in zip(*self.)]
