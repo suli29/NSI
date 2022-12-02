24e21acf045e969128390ac452319ada01cbcdc2
J3anDavid <115627033+J3anDavid@users.noreply.github.com>
2022-12-02 10:41:12 +0100

    Update 2048 textuel.py

--- projet terminal/projet 1/2048 textuel.py
+++ projet terminal/projet 1/2048 textuel.py
@@ -1,8 +1,15 @@
-plateau = [[2,0,4,0],[0,128,0,4],[0,8,0,32],[0,2,0,0]]
+plateau = [[2,0,4,0],[0,16,0,4],[0,8,0,32],[0,2,0,0]]
+
 taille_plateau = 4
 
 def affichage():
-    
+    """
+    Fonction qui permet d'afficher le plateau de jeu
+    param ligne: ligne du plateau
+    param element: numero dans la case
+    param larg: largeur du tableau
+    param NumSpace: Nombre d'espace
+    """
     larg = plateau[0][0]
     for ligne in plateau:
         for element in ligne:
@@ -24,6 +31,12 @@ def affichage():
     print()
 
 def fusionligne1(ligne):
+    """
+    Fonction qui permet de fusionner la ligne
+    param j :(int) ordonnee soit la colonne
+    param i :(int) abscisse soit la ligne
+    return : retourne la ligne
+    """
     for j in range(taille_plateau -1):
         for i in range(taille_plateau -1, 0, -1):
             if ligne[i -1] == 0:
@@ -39,9 +52,15 @@ def fusionligne1(ligne):
             ligne[i-1] == ligne[i]
             ligne[i] == 0
     return ligne
-
+            
 
 def fusion_g(cb):
+    """
+    Fonction qui permet de fusionner vers la gauche
+    param i : abscisse soit la ligne
+    param cb : variable qui signifie le plateau actuel
+    return : retourne la variable cb
+    """
     for i in range (taille_plateau):
         cb[i] = fusionligne1(cb[i])    
     return cb
@@ -51,14 +70,22 @@ def reverse(ligne):
     for i in range (taille_plateau -1,-1,-1):
         new.append(ligne[i])
     return new
-        
+
 def fusion_d(cb):
+    """
+    Fonction qui permet de fusionner vers la droite
+    param i : abscisse soit la ligne
+    param cb : variable qui signifie le plateau actuel
+    return : retourne la variable cb
+    """
     for i in range (taille_plateau):
         cb[i] = reverse(cb[i])
         cb[i] = fusionligne1(cb[i])
         cb[i] = reverse(cb[i])
     return cb
-    
+            
+            
+            
 def transpose(cb):
     """
     Fonction qui permet transposer i vers j ou inversement
@@ -73,7 +100,7 @@ def transpose(cb):
                 cb[j][i] = cb[i][j]
                 cb[i][j] = temp
     return cb
-    
+            
 def fusionhaut(cb):
     """
     Fonction qui permet de fusionner vers le haut
@@ -96,19 +123,15 @@ def fusionbas(cb):
     cb = fusion_d(cb)   #permet de fusionner a droite
     cb = transpose(cb) #permet de transposer
     
-    return cb 
-
-
-
-
-
+    return cb
+    
+            
+            
+            
 
 fusion_g(plateau)
-affichage()                 
-                    
-                    
-
-                
-
-
-
+fusionhaut(plateau)
+fusion_d(plateau)
+fusionhaut(plateau)
+fusion_g(plateau)
+affichage()
