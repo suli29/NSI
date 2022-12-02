2204ead736a239d6bb5f82779e87660cb76bc06f
Killu <115620592+suli29@users.noreply.github.com>
2022-12-02 09:35:42 +0100

    Add files via upload

--- projet terminal/projet 1/2048 textuel.py
+++ projet terminal/projet 1/2048 textuel.py
@@ -1,18 +1,72 @@
 plateau = [[2,0,4,0],[0,16,0,4],[0,8,0,32],[0,2,0,0]]
 
 def affichage():
+    
+    larg = plateau[0][0]
+    for ligne in plateau:
+        for element in ligne:
+            if element > larg :
+                larg = element
+    
+    NumSpaces = len(str(larg))
+                
+    
     for ligne in plateau:
         c ="|"
         for element in ligne:
             if element == 0:
-                c += "|"
+                c += " " * NumSpaces +"|"
+            
             else :
-                c += str(element) + "|"
+                c += (" " *(NumSpaces - len(str(element)))) + str(element) + "|"
         print(c)
     print()
-                    
-                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 affichage()
+
+
+
+
+
+
 
                     
                     
